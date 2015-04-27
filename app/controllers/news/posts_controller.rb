4e08220dfab979f72cacafb6909f723d992034d3
Artem Kolesnikov <sirruf@me.com>
2015-04-27 04:43:00 +0300

    0.0.10

--- app/controllers/news/posts_controller.rb
+++ app/controllers/news/posts_controller.rb
@@ -8,7 +8,7 @@ module News
 
 
     def show
-      @meta_keywords = @post.meta_keywords if @page.meta_keywords.present?
+      @meta_keywords = @post.meta_keywords if @post.meta_keywords.present?
       @title = @post.title
     end
 
