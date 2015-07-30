ebc21641c949c8b4064c09ddf427a35189c81f67
Artem Kolesnikov <sirruf@me.com>
2015-07-30 18:12:26 +0300

    Tests for posts_controller added.

---
+++ spec/controllers/news/admin/posts_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+module News
+  RSpec.describe Admin::PostsController, type: :controller do
+    routes { News::Engine.routes }
+
+    it 'index' do
+      get :index
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('index')
+    end
+
+    it 'new' do
+      get :new
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('new')
+    end
+
+    it 'show' do
+      post = FactoryGirl.create :post
+      get :show, id: post.id
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('show')
+    end
+
+    it 'edit' do
+      post = FactoryGirl.create :post
+      get :edit, id: post.id
+      expect(response).to have_http_status(200)
+      expect(response).to render_template('edit')
+    end
+
+  end
+end
+
+
